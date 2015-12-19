901e0743ed9d786347cce7e90f4ad64b172a5231
Kimberly Soares <kimberly.soares02@gmail.com>
2015-12-19 01:16:21 -0500

    Inbox

--- cheapoInbox.php
+++ cheapoInbox.php
@@ -9,7 +9,10 @@
     $count = 0; 
    
     $userid = $_GET["id"];
-    $sql = "SELECT * FROM Message";
+    //$last = $_GET["last"];
+    //$time = $_GET["currentTime"];
+
+    $sql = "SELECT * FROM Message ORDER BY id DESC LIMIT 11"; // Last 10 msgs
     $query = mysql_query($sql);
 
     if ($query === false) {
@@ -26,7 +29,7 @@
             // Print table
             echo "<table class='table table-mail' id='inboxMsgs'>"; 
             echo "<tbody>";
-            echo "<td style= 'display: none' id= 'numRows'>$num_rows</td>"; // Number of records in table
+            echo "<td style= 'display: none' id= 'numRows'>$num_rows</td>"; // Number of records in db table
 
             while($row = mysql_fetch_array($query))
             {
@@ -47,9 +50,15 @@
                                     exit;
                                  }
                                 else if (!mysql_num_rows($results) > 0) {
-                                        // Record found therefore message has been read before
+                                        // Record not found therefore message has been read before
+                                        $unread = true;
                                         echo "unread"; // Class
                                     }
+                                // Indicate whether new
+                                // Compare times (H:M:S)
+                                ////$loginTime = explode(":",$_SESSION["loginTime"]);
+                                //$currentTime = explode(":",$time);
+                            
                         ?>" id="msg$count"> <?php
                             echo "<td class='check-mail'>";
                                 echo "<div>";
@@ -77,8 +86,9 @@
 
                         echo "<td class='mail-subject' id= 'subject$count'><a href='#' onclick='readMsg($count);'>$row[subject]</a></td>"; // Subject
                         echo "<td class style='width: 30px'></td>";
-                        echo "<td class='mail-date' id= 'statusMsg$count'><a href='#' onclick='readMsg($count);'>";
-                        ?><?php //Determine record status
+                        ?>
+                        <td class="mail-date <?php if (($row[id] > 10) && ($unread)){ echo " new";}?>" id= "statusMsg$count"><a href="#" onclick="readMsg(<?php echo $count; ?>);">
+                        <?php //Determine record status
                                  $esql = "SELECT date FROM message_read WHERE message_id = '$row[id]'";
                                  $result = mysql_query($esql);
                                  if ($result === false) {
